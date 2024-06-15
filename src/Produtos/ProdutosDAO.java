91c6baa7b47c17589f7e911bea820f083d6295af
João Victor <jvna07@gmail.com>
2024-06-15 13:41:01 -0300

    Correção de bug em cadastrarProduto()

--- src/Produtos/ProdutosDAO.java
+++ src/Produtos/ProdutosDAO.java
@@ -17,21 +17,22 @@ import java.util.ArrayList;
 
 public class ProdutosDAO {
     
-    Connection conn;
-    PreparedStatement prep;
+    private conectaDAO conexao;
+    private Connection conn;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
+    public ProdutosDAO(){
+        this.conexao = new conectaDAO();
+        this.conn = conexao.connectDB();
+    }
+    
     public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        conn = new conectaDAO().connectDB();
-        String sql = ("INSERT INTO produtos(nome, valor, status) VALUES (?, ?, ?)");
+        String sql = ("INSERT INTO produtos(nome, valor) VALUES (?, ?)");
         try{
-            prep = conn.prepareStatement(sql);
+            PreparedStatement prep = this.conn.prepareStatement(sql);
             prep.setString(1, produto.getNome());
             prep.setInt(2, produto.getValor());
-            prep.setString(3, produto.getStatus());
             prep.execute();
         }catch(Exception e){
             System.out.println("Erro ao inserir produto:\n" + e.getMessage());
