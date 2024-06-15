ef29ae25e5c35a3e672eade3202104acfd6f1c80
João Victor <jvna07@gmail.com>
2024-06-15 13:17:07 -0300

    Funcionalidade salvar cadastro

--- src/Produtos/ProdutosDAO.java
+++ src/Produtos/ProdutosDAO.java
@@ -25,7 +25,17 @@ public class ProdutosDAO {
     public void cadastrarProduto (ProdutosDTO produto){
         
         
-        //conn = new conectaDAO().connectDB();
+        conn = new conectaDAO().connectDB();
+        String sql = ("INSERT INTO produtos(nome, valor, status) VALUES (?, ?, ?)");
+        try{
+            prep = conn.prepareStatement(sql);
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+            prep.execute();
+        }catch(Exception e){
+            System.out.println("Erro ao inserir produto:\n" + e.getMessage());
+        }
         
         
     }
