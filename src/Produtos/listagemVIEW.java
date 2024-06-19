1442127e43013aa9c0911a5e3f45c0065b8b9e4d
João Victor <jvna07@gmail.com>
2024-06-19 20:12:36 -0300

    Botão para vender produto

--- src/Produtos/listagemVIEW.java
+++ src/Produtos/listagemVIEW.java
@@ -143,7 +143,7 @@ public class listagemVIEW extends javax.swing.JFrame {
         
         ProdutosDAO produtosdao = new ProdutosDAO();
         
-        //produtosdao.venderProduto(Integer.parseInt(id));
+        produtosdao.venderProdutos(Integer.parseInt(id));
         listarProdutos();
     }//GEN-LAST:event_btnVenderActionPerformed
 
